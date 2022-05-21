b4b07ff7415f9914c51cd87911ddf440f6735b2d
Hélio Toshio Kamakawa <heliokamakawa@gmail.com>
2022-05-20 22:24:48 -0300

    Add files via upload

---
+++ 09b - lista de exercícios group by.sql
@@ -0,0 +1,16 @@
+/*
+EXERCÍCIOS
+01) Listar o nome de cidade e quantos fornecedores tem.
+02) Listar o nome de cidade e quantos clientes tem.
+03) Listar o nome de cidade e quantos funcionários tem.
+04) Listar o nome de cidade e os nomes dos fornecedores de cada cidade.
+05) Listar o nome de estado e quantos clientes tem. 
+06) Listar o nome de cidade e os nomes dos funcionários de cada cidade 
+07) Listar o nome de estado e quantos funcionários tem em cada estado. Mostre somente os estados que apresentam mais que 3 funcionários.
+08) Listar o nome de estado e quantos clientes tem em cada estado. Mostre somente os estados da região sul e que apresentam mais que 5 clientes em cada estado.
+09) Listar o nome das categorias dos produtos que nunca foram vendidos.
+10) Listar o nome das categorias dos produtos e quantos produtos tem em cada categoria. Apresente somente as categorias que tem a quantidade de prodtuos maior que a média da categoria.
+11) De acordo com o script executado, elabore 5 consultas utilizando GROUP BY que podem ajudar a empresa. Exeplique.
+12) De acordo com o seu estudo de caso, elabore 5 consultas utilizando GROUP BY que podem ajudar a empresa. Exeplique.
+13) De acordo com o seu estudo de caso, faça pelo menos 3 exemplos de consultas utilizando GROUP BY e LEFT/RIGHT JOIN que podem ajudar a empresa. Exeplique.
+*/
