2d34e89bf6b80059b211ab916c80f87aa7ec8953
Hélio Toshio Kamakawa <heliokamakawa@gmail.com>
2022-05-20 23:25:55 -0300

    Add files via upload

---
+++ 11d- SUBQUERY - lista de exercícios.sql
@@ -0,0 +1,9 @@
+
+/*
+01)Utilizando subquery (sem utilizar o JOIN), liste todas as cidades e seus respectivos estados. 
+02)Em relação ao exercício anterior, qual a diferença em relação ao JOIN? Qual possui o  melhor desempenho? Justifique.
+03)O subquery pode ser utilizando em quais comandos DML? Em quais cláusulas de uma consulta? 
+04)Utilizando subquery, liste o nome dos estados e a quantidade de cidades que cada cidade tem (não use o GROUP BY).
+05)Liste o nome dos clientes que já compraram algum produto na empresa.
+06)Liste o nome de produtos que nunca foram vendidos.
+*/
