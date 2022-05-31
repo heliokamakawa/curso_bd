288f55588f94b8df92df473d5881480e3cc10ead
Hélio Toshio Kamakawa <heliokamakawa@gmail.com>
2022-05-31 20:53:54 -0300

    Update Lista de exercícios complementar 06 (JOIN, OUTER JOIN, GROUP BY, VIEW, SUBQUERY).sql

--- Lista de exercícios complementar 06 (JOIN, OUTER JOIN, GROUP BY, VIEW, SUBQUERY).sql
+++ Lista de exercícios complementar 06 (JOIN, OUTER JOIN, GROUP BY, VIEW, SUBQUERY).sql
@@ -9,5 +9,5 @@ Link Script: https://github.com/heliokamakawa/curso_bd/blob/master/der/servico%2
 -- 3) Liste a data de cadastro e o total de todas as ordens de serviços e caso exista, os respectivos serviços realizados.
 -- 4) Crie uma VIEW que apresente todos os clientes ativos do estado “PARANÁ” que já realizam algum orçamento. Em seguida, escreva o comando de consulta desta VIEW que apresente o nome de todos os clientes que fizeram o orçamento acima de R$ 500,00.
 -- 5) Liste os nomes de todos os clientes e o respectivo total geral de todas as ordens de serviços realizadas pelo respectivo cliente. Retire os resultados em que a média de total geral de cada cliente seja abaixo de R$ 900,00.
--- 6) Liste o nome e o preço >>de venda<< de todas as peças cadastradas que já foram orçadas pelos clientes, porém que ainda não foram vendidas.
+-- 6) Liste o nome e o preço de venda de todas as peças cadastradas que já foram orçadas pelos clientes, porém que ainda não foram vendidas.
 
